d211a0221340adedbd0f5982515f726ef9ee7368
이승윤 <syoon0624@khu.ac.kr>
2021-06-20 22:54:35 +0900

    feat: 게시판 수정 기능

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('blog', views.post_list, name = 'post_list'),
     path('blog/post/<int:pk>/', views.post_detail, name='post_detail'),
     path('blog/post/new', views.post_new, name='post_new'),
+    path('blog/post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
