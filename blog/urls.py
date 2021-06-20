f8fe0570fab26c134522ecd41e8469d1a4a9781b
이승윤 <syoon0624@khu.ac.kr>
2021-06-20 22:33:06 +0900

    feat: 게시글 추가 버튼 기능

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main_page, name = 'main_page'),
     path('blog', views.post_list, name = 'post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog/post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog/post/new', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -14,3 +15,16 @@ def post_detail(request,pk):
 def main_page(request):
     m_views = 'hello'
     return render(request, 'blog/main_page.html', {'views': m_views})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
