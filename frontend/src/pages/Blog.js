3bdbedf077ffe041777b9b1c97ba988156bf0c98
seungyoonLee <syoon624@naver.com>
2021-09-13 17:25:15 +0900

    feat: post 리스트 컨테이너 추가

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Sidebar from '../components/sidebar';
+import TextItem from './TextItem';
+import axios from 'axios';
 
-const Main = styled.div`
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+const Main = styled.div``;
+
+const Container = styled.div``;
+
+const Postwrap = styled.ul`
+  padding-left: 8%;
+  padding-right: 8%;
   display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Container = styled.div`
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  flex-wrap: wrap;
 `;
 
 const BlogPage = () => {
+  const [state, setState] = useState([]);
+
+  const _renderText = () => {
+    axios.get('/api/post/').then(res => {
+      setState(res.data);
+    });
+  };
+
+  if (state.length === 0) {
+    _renderText();
+  }
+
   return (
     <Container>
       <Sidebar />
-      <Main>This is Blog Page.</Main>
+      <Main>
+        <div>
+          <button onClick={_renderText}>불러오기</button>
+        </div>
+        <Postwrap>
+          {state.map((text, index) => {
+            return (
+              <TextItem
+                title={text.title}
+                key={index}
+                id={text.id}
+                author={text.author}
+                created_date={text.created_date.substring(0, 10)}
+                published_date={text.published_date.substring(0, 10)}
+              />
+            );
+          })}
+          <TextItem
+            title="제목"
+            id="id"
+            author="저자"
+            created_date="날짜"
+            published_date="수정일"
+          />
+          <TextItem
+            title="제목"
+            id="id"
+            author="저자"
+            created_date="날짜"
+            published_date="수정일"
+          />
+        </Postwrap>
+      </Main>
     </Container>
   );
 };
--- frontend/src/pages/TextItem.js
+++ frontend/src/pages/TextItem.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const TextItem = ({ text, id, handleClick }) => {
+const PostLi = styled.li`
+  list-style: none;
+  margin: 20px;
+  width: 350px;
+  height: 300px;
+  border-radius: 30px;
+  border: 2px solid black;
+  box-shadow: #339af0 5px 5px;
+  padding: 15px;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  color: #748ffc;
+  border-bottom: 2px solid #63e6be;
+`;
+
+const TextItem = ({ title, id, author, created_date, published_date }) => {
   return (
-    <div>
-      <p>{text}</p>
-      <button onClick={() => handleClick(id)}>삭제</button>
-    </div>
+    <PostLi>
+      <Title>제목 : {title}</Title>
+      <p>작성자 : {author}</p>
+      <p>작성일 : {created_date}</p>
+      <p>최근 수정일 : {published_date}</p>
+      <Link to={'/' + id}>더보기</Link>
+    </PostLi>
   );
 };
 
